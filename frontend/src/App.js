0e57dbd3d556d3d1474818436806e14d7ff36f09
pedro.carvalho <pedro.carvalho@clinicasim.com>
2022-05-15 16:36:21 -0300

    adding header

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,39 @@
-import logo from './logo.svg';
 import './App.css';
+
+let y = []
+
+for(let i = 0; i <= 100; i++) {
+  y.push(i)
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <main className="w-100 h-100">
+      <header className="relative bg-white">
+        <div>
+          <div className="flex justify-between items-center border-b-2 border-gray-100 py-6 md:justify-start md:space-x-10 px-8">
+            <div className="flex justify-start lg:w-0 lg:flex-1">
+              <a href="#">
+                <span className="sr-only">Workflow</span>
+                <img className="h-8 w-auto sm:h-10" src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg" alt=""/>
+              </a>
+            </div>
+            
+            <div className="hidden md:flex items-center justify-end md:flex-1 lg:w-0">
+              <a href="#" className="whitespace-nowrap text-base font-medium text-gray-500 hover:text-gray-900"> Sign in </a>
+              <a href="#" className="ml-8 whitespace-nowrap inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700"> Sign up </a>
+            </div>
+          </div>
+        </div>
       </header>
-    </div>
+
+      {
+        y.map((a) => {
+          return <p>teste</p>
+        })
+      }
+      
+    </main>
   );
 }
 
