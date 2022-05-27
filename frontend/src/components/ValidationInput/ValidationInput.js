e5ed377a5218487abf1f4e31b5e05b1021fb95fc
JsBatista <joaobatistafreitasfilho@gmail.com>
2022-05-27 14:05:27 -0300

    Adicionando versao nao estilizada de ValidationInput

---
+++ frontend/src/components/ValidationInput/ValidationInput.js
@@ -0,0 +1,28 @@
+import {useState} from 'react';
+import styles from "./ValidationInput.module.css";
+
+/**
+ * Componente que representa um campo de input que pode ser validado.
+ *
+ * props:
+ *  * label (String): Texto que indica sobre o que é o campo 
+ *  * hint (String): Dica que aparece quando o campo está vazio
+ *  * type (String): Tipo do input
+ *  *       <ValidationInput label="Código da Disciplina" hint="ex: CK0203" type="text" inputRef={disciplineHoursRef} validation={validationNumberChecker} />
+ (Ref): Referência do valor do campo
+ *  * validation (Function): Função que faz a validação do input e retorna uma mensagem indicando o possível erro de validação.
+ *      Retorno da função validation: Object {  isValid: Boolean,  message: String (opcional)  }
+ */
+function ValidationInput(props) {
+  let [shouldValidate, setShouldValidate] = useState(false);
+
+  return (
+    <div>
+      <label htmlFor='validationInput'>{props.label}:</label>
+      <input className={styles.validationInput} type={props.type} id='validationInput' onFocus={() => setShouldValidate(false)} onBlur={() => setShouldValidate(true)} ref={props.inputRef} />
+      {shouldValidate && !props.validation(props.inputRef).isValid && <small className={styles.validationError}>{props.validation(props.inputRef).message || 'Campo inválido!'}</small>}
+    </div>
+  );
+}
+
+export default ValidationInput;
--- frontend/src/pages/DisciplineRegisterPage/DisciplineRegisterPage.js
+++ frontend/src/pages/DisciplineRegisterPage/DisciplineRegisterPage.js
@@ -1,7 +1,34 @@
+import { useRef } from "react";
+import ValidationInput from "../../components/ValidationInput/ValidationInput";
+
 function DisciplineRegisterPage() {
+
+  const disciplineNameRef = useRef(null);
+  const disciplineCodeRef = useRef(null);
+  const disciplineHoursRef = useRef(null);
+
+  const validationStringChecker = (inputRef) => {
+    if(inputRef && inputRef.current && inputRef.current.value && inputRef.current.value.length >= 0){
+      return { isValid: true}    
+    }
+    return { isValid: false, message:'Esse campo não pode estar vazio' }
+  }
+
+  const validationNumberChecker = (inputRef) => {
+    if(inputRef && inputRef.current && inputRef.current.value){
+      if(Number.isNaN(inputRef.current.value) || inputRef.current.value <= 0)
+        return { isValid: false, message:'Valor numérico inválido!' }
+      return { isValid: true}    
+    }
+    return { isValid: false, message:'Esse campo não pode estar vazio' }
+  }
+
   return (
     <div>
       <h1>DisciplineRegisterPage</h1>
+      <ValidationInput label="Nome da Disciplina" hint="ex: Algoritmos Aproximativos" type="text" inputRef={disciplineNameRef} validation={validationStringChecker} />
+      <ValidationInput label="Código da Disciplina" hint="ex: CK0203" type="text" inputRef={disciplineCodeRef} validation={validationStringChecker} />
+      <ValidationInput label="Carga horária da Disciplina" hint="ex: 96" type="number" inputRef={disciplineHoursRef} validation={validationNumberChecker} />
     </div>
   );
 }
