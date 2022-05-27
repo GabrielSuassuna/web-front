5f6b0a7eac467c1d373e98e451a9dfa9bce5f4d4
JsBatista <joaobatistafreitasfilho@gmail.com>
2022-05-27 15:18:43 -0300

    Ajustando comentário do ValidationInput

--- frontend/src/components/ValidationInput/ValidationInput.js
+++ frontend/src/components/ValidationInput/ValidationInput.js
@@ -9,7 +9,6 @@ import styles from "./ValidationInput.module.css";
  *  * hint (String): Dica que aparece quando o campo está vazio
  *  * name (String): Nome do input
  *  * type (String): Tipo do input
- *  *       <ValidationInput label="Código da Disciplina" hint="ex: CK0203" type="text" inputRef={disciplineHoursRef} validation={validationNumberChecker} />
  *  * inputRef (Ref): Referência do valor do campo
  *  * validation (Function): Função que faz a validação do input e retorna uma mensagem indicando o possível erro de validação.
  *      Retorno da função validation: Object {  isValid: Boolean,  message: String (opcional)  }
