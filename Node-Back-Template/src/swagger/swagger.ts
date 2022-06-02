497f0a96db02f26b556aeb2cb9eecc6d20793449
JsBatista <joaobatistafreitasfilho@gmail.com>
2022-06-02 12:34:03 -0300

    Alterando título do swagger

--- Node-Back-Template/src/swagger/swagger.ts
+++ Node-Back-Template/src/swagger/swagger.ts
@@ -64,7 +64,7 @@ export const swaggerDocument = {
     "openapi": "3.0.1",
     "info": {
         "version": "1.0.0",
-        "title": "Web API Document",
+        "title": "Documentação - Sistema de Feedback Universitário - Rotas",
         "license": {
           "name": 'Apache 2.0',
           "url": 'https://www.apache.org/licenses/LICENSE-2.0.html'
