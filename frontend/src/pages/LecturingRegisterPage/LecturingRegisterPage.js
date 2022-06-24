f3da4216731409288944e5132bfb3f1de343c997
JsBatista <joaobatistafreitasfilho@gmail.com>
2022-06-24 16:05:04 -0300

    Adicionando funcionalidade de registro de disciplina ministrada

--- frontend/src/pages/LecturingRegisterPage/LecturingRegisterPage.js
+++ frontend/src/pages/LecturingRegisterPage/LecturingRegisterPage.js
@@ -1,7 +1,110 @@
+import { useRef, useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import IconButton from "../../components/IconButton/IconButton";
+import ValidationInput from "../../components/ValidationInput/ValidationInput";
+import ValidationSelect from "../../components/ValidationSelect/ValidationSelect";
+import useSWR from "swr";
+import fetcher from "../../utils/fetcher";
+import { apiRequest } from "../../utils/apiReq";
+import { DUMMY_AUTH_TOKEN } from "../../utils/consts";
+
 function LecturingRegisterPage() {
+  const navigate = useNavigate();
+
+  let [professorOptions, setProfessorOptions] = useState([]);
+  let [disciplineOptions, setDisciplineOptions] = useState([]);
+  let [professorLoaded, setProfessorLoaded] = useState(false);
+  let [disciplineLoaded, setDisciplineLoaded] = useState(false);
+
+  const [professor, setProfessor] = useState(null);
+  const [discipline, setDiscipline] = useState(null);
+
+  const { data: professors, error: professorsError } = useSWR(
+    `http://localhost:3000/professor/`,
+    fetcher
+  );
+  const { data: disciplines, error: disciplinesError } = useSWR(
+    `http://localhost:3000/discipline/`,
+    fetcher
+  );
+
+  useEffect(() => {
+    if (professors && professors.data && !professorLoaded) {
+      let professorOptions = professors.data.map((f) => {
+        console.log(f);
+        return {
+          value: f.id,
+          label: `${f.siape} - ${f.name}`,
+        };
+      });
+      setProfessorOptions(professorOptions);
+      setProfessorLoaded(true);
+    }
+    if (disciplines && disciplines.data && !disciplineLoaded) {
+      let disciplineOptions = disciplines.data.map((f) => {
+        console.log(f);
+        return {
+          value: f.id,
+          label: `${f.code} - ${f.name}`,
+        };
+      });
+      setDisciplineOptions(disciplineOptions);
+      setDisciplineLoaded(true);
+    }
+  }, [
+    professorOptions,
+    disciplineOptions,
+    professors,
+    disciplines,
+    professorLoaded,
+    disciplineLoaded,
+  ]);
+
+  const registerLecturingHandler = async () => {
+    
+    let requestData = {
+      professorId: professor,
+      disciplineId: discipline,
+    };
+
+    apiRequest(
+      'POST',
+      'http://localhost:3000/lecturing/',
+      requestData,
+      (res) => {
+        alert("Registro de disciplina ministrada realizado!")
+        console.log(res)
+        navigate('/')
+      },
+      (res) => {
+        alert(res.message)
+        console.log(res.message)
+        console.log(res.errorStack)
+      },
+      DUMMY_AUTH_TOKEN
+    );
+  };
+
   return (
     <div>
-      <h1>LecturingRegisterPage</h1>
+      <h1>Registrar disciplina ministrada por professor</h1>
+      <ValidationSelect
+        name="professor"
+        label="Professor"
+        hint="Selecione um professor"
+        value={professor}
+        valueHandler={setProfessor}
+        options={professorOptions}
+      />
+      <ValidationSelect
+        name="discipline"
+        label="Discipline"
+        hint="Selecione uma disciplina"
+        value={discipline}
+        valueHandler={setDiscipline}
+        options={disciplineOptions}
+      />
+      <IconButton content="Registrar-se" onClick={registerLecturingHandler} />
     </div>
   );
 }
