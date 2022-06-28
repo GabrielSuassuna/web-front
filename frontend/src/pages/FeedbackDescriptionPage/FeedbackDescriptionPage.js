31c385cf741447d5afa009ea3de50c194c2484f5
JsBatista <joaobatistafreitasfilho@gmail.com>
2022-06-28 10:39:07 -0300

    Adicionando lógica de obter interação na descrição do feedback

--- frontend/src/pages/FeedbackDescriptionPage/FeedbackDescriptionPage.js
+++ frontend/src/pages/FeedbackDescriptionPage/FeedbackDescriptionPage.js
@@ -1,18 +1,30 @@
 import useQuery from "../../hooks/useQuery";
 import useSWR from "swr";
 import fetcher from "../../utils/fetcher";
+import {DUMMY_STUDENT_ID} from "../../utils/consts";
 
 function FeedbackDescriptionPage() {
 
   let query = useQuery();
 
-  const {data, error} = useSWR(`http://localhost:3000/feedback/${query.get("id")}`, fetcher);
+  const {data: feedback, error: feedbackError } = useSWR(`http://localhost:3000/feedback/${query.get("id")}`, fetcher);
+  const {data: hasVote, error: hasVoteError } = useSWR(`http://localhost:3000/hasVote?studentId=${DUMMY_STUDENT_ID}&feedbackId=${query.get("id")}`, fetcher);
+  
+
+  if(!hasVote){
+    return (
+      <div>
+        <h1>Carregando...</h1>
+      </div>
+    );  
+  }
 
   return (
     <div>
       <h1>FeedbackDescriptionPage</h1>
-      <p>{JSON.stringify(data)}</p>
-      <p>{JSON.stringify(error)}</p>
+      <p>{JSON.stringify(feedback)}</p>
+      <hr/>
+      <p>{JSON.stringify(hasVote)}</p>
     </div>
   );
 }
