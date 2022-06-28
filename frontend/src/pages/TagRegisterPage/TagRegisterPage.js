ae943a3610b1cbc36b7534d4dec9e3abe774df5c
JsBatista <joaobatistafreitasfilho@gmail.com>
2022-06-28 09:57:52 -0300

    Adicionando página de registro de tag

--- frontend/src/pages/TagRegisterPage/TagRegisterPage.js
+++ frontend/src/pages/TagRegisterPage/TagRegisterPage.js
@@ -1,7 +1,71 @@
+import { useRef } from "react";
+import { useNavigate } from "react-router-dom";
+import IconButton from "../../components/IconButton/IconButton";
+import ValidationInput from "../../components/ValidationInput/ValidationInput";
+import styles from "./TagRegisterPage.module.css";
+import { DUMMY_AUTH_TOKEN } from "../../utils/consts";
+import { apiRequest } from "../../utils/apiReq";
+import { validationStringChecker } from "../../utils/validation";
+
 function TagRegisterPage() {
+  const navigate = useNavigate();
+
+  const tagNameRef = useRef(null);
+  const tagDescriptionRef = useRef(null);
+
+  const registerTagHandler = async () => {
+    if (
+      !validationStringChecker(tagNameRef).isValid ||
+      !validationStringChecker(tagDescriptionRef).isValid
+    )
+      return alert("Dados inválidos!");
+
+    let requestData = {
+      name: tagNameRef.current.value,
+      description: tagDescriptionRef.current.value,
+    };
+
+    apiRequest(
+      'POST',
+      "http://localhost:3000/tag/",
+      requestData,
+      (res) => {
+        alert("Registro de tag realizado!");
+        console.log(res);
+        navigate("/");
+      },
+      (res) => {
+        alert(res.message);
+        console.log(res.message);
+        console.log(res.errorStack);
+      },
+      DUMMY_AUTH_TOKEN
+    );
+  };
+
   return (
     <div>
-      <h1>TagRegisterPage</h1>
+      <h1>Cadastrar nova Tag (Característica)</h1>
+
+      <ValidationInput
+        label="Nome"
+        hint="ex: Aulas objetivas"
+        type="text"
+        name="name"
+        inputRef={tagNameRef}
+        validation={validationStringChecker}
+      />
+      <ValidationInput
+        label="Descrição"
+        hint="ex: O docente é muito direto em suas aulas, não perdendo o tempo com conversas paralelas ou perdendo o foco durante a aula."
+        type="text"
+        name="description"
+        inputRef={tagDescriptionRef}
+        validation={validationStringChecker}
+        inputClasses={[styles.inputQuestion]}
+        isTextArea
+      />
+      <IconButton content="Registrar Tag" onClick={registerTagHandler} />
     </div>
   );
 }
