c9b98a6071db11d7868606ee3bceeb9df09cd13c
Davi <davispcosta@gmail.com>
2022-07-04 17:38:43 -0300

    disciples descriptions

--- frontend/src/pages/DisciplineDescriptionPage/DisciplineDescriptionPage.js
+++ frontend/src/pages/DisciplineDescriptionPage/DisciplineDescriptionPage.js
@@ -16,13 +16,40 @@ function DisciplineDescriptionPage() {
 
   checkForErrors([disciplineError]);
 
+  if (!discipline) {
+    return (
+      <div>
+        <h1>Carregando...</h1>
+      </div>
+    );
+  }
+
   return (
-    <div>
-      <h1>DisciplineDescriptionPage</h1>
-      <p>{JSON.stringify(discipline)}</p>
-      <h2>Tags Populares:</h2>
-      <Tag id="ID_tag_aulasObjetivas" title="Aulas Objetivas" />
-      <Tag id="ID_tag_visaoEstrategica" title="Visao Estratégica" />
+    <div className="mt-6 ml-6">
+      <h1 className="text-4xl font-bold mb-6">{discipline.data[0].name}</h1>
+      <p>{discipline.data[0].description}</p>
+      <div className="flex flex-row mt-3">
+        <p className="font-bold mr-2">Código:</p>
+        <p>{discipline.data[0].code}</p>
+      </div>
+      <div className="flex flex-row mt-3">
+        <p className="font-bold mr-2">Carga Horário:</p>
+        <p>{discipline.data[0].hours}h</p>
+      </div>
+      <div className="flex flex-row mt-3">
+        <p className="font-bold mr-2">Número de Feedbacks:</p>
+        <p>{discipline.data[0].feedback_count}</p>
+      </div>
+      <div className="flex flex-row mt-3">
+        <p className="font-bold mr-2">Média Geral:</p>
+        <p>{discipline.data[0].general_score}</p>
+      </div>
+      <h2 className="mt-3 font-bold">Tags Populares:</h2>
+      <div className="pr-4 mt-3 w-56">
+        {discipline.data[0].tags.map((tag) => {
+          return <Tag id={tag.id} title={tag.name} />;
+        })}
+      </div>
     </div>
   );
 }
--- frontend/src/pages/LecturingDescriptionPage/LecturingDescriptionPage.js
+++ frontend/src/pages/LecturingDescriptionPage/LecturingDescriptionPage.js
@@ -1,21 +1,29 @@
 import useQuery from "../../hooks/useQuery";
 import useSWR from "swr";
 import fetcher from "../../utils/fetcher";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { checkForErrors } from "../../utils/apiReq";
 import url from "../../config/api";
+import { getAuthData } from "../../utils/auth";
+import Tag from "../../components/Tag/Tag";
+import { AUTH_LEVELS } from "../../utils/consts";
 
 function LecturingDescriptionPage() {
   let query = useQuery();
+  const navigate = useNavigate();
+
+  let { userType } = getAuthData(navigate);
 
   const { data: lecturing, error: lecturingError } = useSWR(
     `${url}/lecturing/${query.get("id")}`,
     fetcher
   );
+
   const { data: professor, error: professorError } = useSWR(
     () => `${url}/professor/${lecturing.data[0].professor_id}`,
     fetcher
   );
+
   const { data: discipline, error: disciplineError } = useSWR(
     () => `${url}/discipline/${lecturing.data[0].discipline_id}`,
     fetcher
@@ -29,19 +37,65 @@ function LecturingDescriptionPage() {
         <h1>Carregando...</h1>
       </div>
     );
-  }
+  } else {
+    return (
+      <div className="mt-6 ml-6">
+        <h1 className="text-4xl font-bold mb-6">{discipline.data[0].name}</h1>
+        <p>{discipline.data[0].description}</p>
 
-  return (
-    <div>
-      <h1>LecturingDescriptionPage</h1>
-      <p>{JSON.stringify(lecturing)}</p>
-      <p>{JSON.stringify(professor)}</p>
-      <p>{JSON.stringify(discipline)}</p>
-      <Link to={`/register/feedback?lecturingId=${lecturing.data[0].id}`}>
-        FAZER FEEDBACK PARA ESSA DISCIPLINA
-      </Link>
-    </div>
-  );
+        <h2 className="mt-6 font-bold">Tags Populares:</h2>
+        <div className="pr-4 mt-3 w-56">
+          {lecturing.data[0].tags.map((tag) => {
+            return <Tag id={tag.id} title={tag.name} />;
+          })}
+        </div>
+
+        <div className="flex flex-row mt-3">
+          <p className="font-bold mr-2">Código:</p>
+          <p>{discipline.data[0].code}</p>
+        </div>
+        <div className="flex flex-row mt-3">
+          <p className="font-bold mr-2">Carga Horário:</p>
+          <p>{discipline.data[0].hours}h</p>
+        </div>
+        <div className="flex flex-row mt-3">
+          <p className="font-bold mr-2">Número de Feedbacks:</p>
+          <p>{lecturing.data[0].feedback_count}</p>
+        </div>
+        <div className="flex flex-row mt-3">
+          <p className="font-bold mr-2">Média Geral:</p>
+          <p>{lecturing.data[0].average_score}</p>
+        </div>
+
+        <div className="flex flex-row mt-10">
+          <p className="font-bold mr-2 text-lg">Professor:</p>
+          <p className="text-lg">{professor.data[0].name}</p>
+        </div>
+
+        <div className="flex flex-row mt-1">
+          <p>{professor.data[0].about}</p>
+        </div>
+
+        <div className="flex flex-row mt-3">
+          <p className="font-bold mr-2">Lattes:</p>
+          <p>{professor.data[0].lattes_url}</p>
+        </div>
+
+        <div className="flex flex-row mt-3">
+          <p className="font-bold mr-2">Departamento:</p>
+          <p>{professor.data[0].department_name}</p>
+        </div>
+
+        {userType === AUTH_LEVELS.STUDENT ? (
+          <Link to={`/register/feedback?lecturingId=${lecturing.data[0].id}`}>
+            <div className="mt-6 w-96 text-white rounded px-2 text-center py-2 bg-indigo-700">
+              FAZER FEEDBACK PARA ESSA DISCIPLINA
+            </div>
+          </Link>
+        ) : null}
+      </div>
+    );
+  }
 }
 
 export default LecturingDescriptionPage;
